28f503c88fb623e47461e68c996bf69e1d5d255e
xiaowen <xiaowenlaile@gmail.com>
2025-01-19 10:02:09 +0100

    add disable-firewall.sh

---
+++ disable-firewall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# https://www.youtube.com/@xiaowenlaile
+set -euo pipefail
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script requires root privileges."
+  exit 1
+fi
+
+if command -v ufw &> /dev/null; then
+    ufw disable
+elif command -v iptables &> /dev/null; then
+    iptables -F
+    iptables -X
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables-save > /etc/iptables/rules.v4
+fi
+
+echo "OK"
